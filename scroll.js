e9c67c8005b7cbd89ea3f965da7d1c1cefef43c4
Jenny Zhang <95259224+jxnnyzhang@users.noreply.github.com>
2023-07-02 17:15:09 -0400

    Delete scroll.js

--- scroll.js
+++
@@ -1,6 +0,0 @@
-$(window).on("scroll", function() {
-    var scrollTop = $(window).scrollTop();
-    $(".tab").css("font-size", 16 + scrollTop / 50 + "px");
-    $(".sidebar-content").css("font-size", 16 + scrollTop / 100 + "px");
-  });
-  
